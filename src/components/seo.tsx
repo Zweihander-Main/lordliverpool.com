81b624541c91e312aa1761469950af6ccd525deb
Zweihänder <zweidev@zweihander.me>
2020-08-04 21:48:17 -0400

    bug(seo.tsx): fix errors from lack of typing

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -5,84 +5,103 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
 
-function SEO({ description, lang, meta, title }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
-        }
-      }
-    `
-  )
+type SEOProps = {
+	description?: string;
+	lang?: string;
+	meta: Array<
+		| {
+				name: string;
+				content: string;
+				property?: undefined;
+		  }
+		| {
+				property: string;
+				content: string;
+				name?: undefined;
+		  }
+	>;
+	title: string;
+};
 
-  const metaDescription = description || site.siteMetadata.description
+type SiteGQLProps = {
+	site: {
+		siteMetadata: {
+			title: string;
+			description: string;
+			author: string;
+		};
+	};
+};
 
-  return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta)}
-    />
-  )
-}
+const SEO: React.FC<SEOProps> = ({
+	description = '',
+	lang = 'en',
+	meta = [],
+	title,
+}) => {
+	const { site } = useStaticQuery<SiteGQLProps>(
+		graphql`
+			query {
+				site {
+					siteMetadata {
+						title
+						description
+						author
+					}
+				}
+			}
+		`
+	);
 
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-}
+	const metaDescription = description || site.siteMetadata.description;
 
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
-}
+	return (
+		<Helmet
+			htmlAttributes={{
+				lang,
+			}}
+			title={title}
+			titleTemplate={`%s | ${site.siteMetadata.title}`}
+			meta={[
+				{
+					name: 'description',
+					content: metaDescription,
+				},
+				{
+					property: 'og:title',
+					content: title,
+				},
+				{
+					property: 'og:description',
+					content: metaDescription,
+				},
+				{
+					property: 'og:type',
+					content: 'website',
+				},
+				{
+					name: 'twitter:card',
+					content: 'summary',
+				},
+				{
+					name: 'twitter:creator',
+					content: site.siteMetadata.author,
+				},
+				{
+					name: 'twitter:title',
+					content: title,
+				},
+				{
+					name: 'twitter:description',
+					content: metaDescription,
+				},
+			].concat(meta)}
+		/>
+	);
+};
 
-export default SEO
+export default SEO;
