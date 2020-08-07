33dd6fb813b6e45b3f301ac09d5e1bcddf6b825d
Zweihänder <zweidev@zweihander.me>
2020-08-06 23:05:37 -0400

    feat(footer): style footer into its own section, remove layout restrictions

--- src/components/structure/footer/index.tsx
+++ src/components/structure/footer/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import styles from './footer.module.scss';
 
 const Footer: React.FC = () => {
-	return <footer>© {new Date().getFullYear()} Martin Hutchinson</footer>;
+	return (
+		<footer className={styles.footer}>
+			© {new Date().getFullYear()} Martin Hutchinson
+		</footer>
+	);
 };
 
 export default Footer;
