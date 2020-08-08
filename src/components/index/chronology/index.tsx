67f3325a7332851ac4dcf60489d568a70283ad62
Zweihänder <zweidev@zweihander.me>
2020-08-07 20:14:53 -0400

    feat(chronology): add in basic outline of index chronology

--- src/components/index/chronology/index.tsx
+++ src/components/index/chronology/index.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import styles from './chronology.module.scss';
 
 const Chronology: React.FC = () => {
-	return null;
+	return (
+		<section className={styles.chronology}>
+			<h1 className={styles.title}>The Life and Times</h1>
+			<span className={styles.line}></span>
+			<div className={styles.timeline}>
+				<div className={styles.chronoItem}>
+					<p className={styles.chronoText}>
+						<strong>July 7</strong>
+						<br /> The birth of Robert Jenkinson.
+					</p>
+					<div className={styles.chronoPoint}></div>
+					<date className={styles.chronoYear} datetime="1770">
+						1770
+					</date>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Chronology;
