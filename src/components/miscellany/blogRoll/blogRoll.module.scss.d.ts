92341fe602d0f5a45648a8393f6e4a3a6800c1c5
Zweihänder <zweidev@zweihander.me>
2020-08-25 00:13:10 -0400

    feat(blogRoll): build out blog roll cards

---
+++ src/components/miscellany/blogRoll/blogRoll.module.scss.d.ts
@@ -0,0 +1,4 @@
+export const masonry: string;
+export const item: string;
+export const header: string;
+export const itemImage: string;
--- src/components/miscellany/blogRoll/index.tsx
+++ src/components/miscellany/blogRoll/index.tsx
@@ -1,8 +1,74 @@
 import React from 'react';
 import styles from './blogRoll.module.scss';
+import { useStaticQuery, graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
 
 const Miscellany: React.FC = () => {
-	return <p>Test</p>;
+	const blogRollData = useStaticQuery<GatsbyTypes.BlogRollQueryQuery>(graphql`
+		query BlogRollQuery {
+			allMarkdownRemark(
+				sort: { order: DESC, fields: [frontmatter___date] }
+				filter: { fields: { sourceInstanceName: { eq: "miscellany" } } }
+			) {
+				edges {
+					node {
+						id
+						fields {
+							slug
+						}
+						frontmatter {
+							title
+							subtitle
+							date(formatString: "MMMM DD, YYYY")
+							featuredImage {
+								childImageSharp {
+									fluid {
+										...GatsbyImageSharpFluid
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	`);
+
+	const { edges: posts } = blogRollData.allMarkdownRemark;
+
+	return (
+		<div className={styles.masonry}>
+			{posts &&
+				posts.map(({ node: post }) => {
+					return post?.fields?.slug ? (
+						<Link to={post.fields.slug}>
+							<article key={post.id} className={styles.item}>
+								{post?.frontmatter?.featuredImage
+									?.childImageSharp?.fluid && (
+									<Img
+										className={styles.itemImage}
+										fluid={
+											post.frontmatter.featuredImage
+												.childImageSharp.fluid
+										}
+									/>
+								)}
+								{post?.frontmatter && (
+									<header className={styles.header}>
+										<h2>{post.frontmatter.title}</h2>
+										{post.frontmatter?.subtitle && (
+											<h3>
+												{post.frontmatter?.subtitle}
+											</h3>
+										)}
+									</header>
+								)}
+							</article>
+						</Link>
+					) : null;
+				})}
+		</div>
+	);
 };
 
 export default Miscellany;
