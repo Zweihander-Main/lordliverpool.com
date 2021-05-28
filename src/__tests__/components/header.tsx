01e62da705e5b81b87fb1ec50e47a586660f663a
Zweihänder <zweidev@zweihander.me>
2021-05-28 19:27:08 -0400

    test(root): halfway setup for testing

--- src/__tests__/components/header.tsx
+++ src/__tests__/components/header.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+
 import Header from 'components/structure/header';
-describe('Header', () => {
-	it('renders correctly', () => {
-		const tree = renderer
-			.create(<Header siteTitle="Default Starter" />)
-			.toJSON();
-		expect(tree).toMatchSnapshot();
-	});
+// You have to write data-testid
+const Title = () => <h1 data-testid="hero-title">Gatsby is awesome!</h1>;
+test('Displays the correct title', () => {
+	const { getByTestId } = render(<Title />);
+	// Assertion
+	expect(getByTestId('hero-title')).toHaveTextContent('Gatsby is awesome!');
+	// --> Test will pass
 });
+// describe('Header', () => {
+// 	it('renders correctly', () => {
+// 		const tree = renderer
+// 			.create(<Header siteTitle="Default Starter" />)
+// 			.toJSON();
+// 		expect(tree).toMatchSnapshot();
+// 	});
+// });
