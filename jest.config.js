e21f55023c2fa05949f5868dd54b2024f7610d9e
Zweihänder <zweidev@zweihander.me>
2021-11-27 16:08:41 -0500

    test(jest): update jest config for runners

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 const { compilerOptions } = require('./tsconfig.json');
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const path = require('path');
+
 const paths = pathsToModuleNameMapper(compilerOptions.paths, {
 	prefix: '<rootDir>/',
 });
@@ -8,21 +10,94 @@ Object.keys(paths).forEach((key) => {
 	fixedPaths[`${key.slice(0, -1)}/(.*)$`] = paths[key].concat('/$1');
 });
 
-module.exports = {
-	transform: {
-		'^.+\\.[jt]sx?$': `<rootDir>/jest-preprocess.js`,
+const basicIgnorePatterns = [
+	'<rootDir>/node_modules',
+	'<rootDir>/\\.cache',
+	'<rootDir>/\\.vscode',
+	'<rootDir>/\\.netlify',
+	'<rootDir>.*/public',
+	'<rootDir>/cypress',
+	'<rootDir>/coverage',
+	'__snapshots__',
+];
+
+const testPathIgnorePatterns = [...basicIgnorePatterns, '__generated__'];
+
+const transformIgnorePatterns = [
+	...basicIgnorePatterns,
+	'__generated__',
+	'<rootDir>/node_modules/(?!(gatsby)/)',
+];
+
+const watchPathIgnorePatterns = [...basicIgnorePatterns];
+
+const common = {
+	testPathIgnorePatterns,
+	transformIgnorePatterns,
+	watchPathIgnorePatterns,
+};
+
+const commonForJestTests = {
+	...common,
+	globals: {
+		__PATH_PREFIX__: '',
 	},
 	moduleNameMapper: {
-		'.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
-		'.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
+		'.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+		'.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+			'<rootDir>/test/__mocks__/file-mock.js',
 		...fixedPaths,
 	},
-	testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
-	transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
-	globals: {
-		__PATH_PREFIX__: ``,
+	setupFiles: ['<rootDir>/test/loadershim.js'],
+	testURL: 'http://localhost',
+	transform: {
+		'^.+\\.[jt]sx?$': '<rootDir>/test/jest-preprocess.js',
 	},
-	testURL: `http://localhost`,
-	setupFiles: [`<rootDir>/loadershim.js`],
-	setupFilesAfterEnv: ['<rootDir>/setup-test-env.js'],
+};
+
+const commonForLintRunners = {
+	...common,
+	testMatch: ['<rootDir>/src/**/*'],
+};
+
+module.exports = {
+	collectCoverageFrom: [
+		'**/src/**/*.{js,jsx,ts,tsx}',
+		'!**/__tests__/**',
+		'!**/__generated__/**',
+		'!**/__server_tests__/**',
+		'!**/node_modules/**',
+	],
+	coverageThreshold: {
+		global: {
+			statements: 15,
+			branches: 10,
+			functions: 15,
+			lines: 15,
+		},
+	},
+	watchPlugins: ['jest-watch-select-projects'] /* TODO add typeahead back */,
+	projects: [
+		{
+			...commonForJestTests,
+			displayName: 'client',
+			testEnvironment: 'jest-environment-jsdom',
+		},
+		{
+			...commonForLintRunners,
+			displayName: 'eslint',
+			runner: 'jest-runner-eslint',
+		},
+		{
+			...commonForLintRunners,
+			displayName: 'prettier',
+			preset: 'jest-runner-prettier',
+		},
+		{
+			...commonForLintRunners,
+			displayName: 'stylelint',
+			preset: 'jest-runner-stylelint',
+			moduleFileExtensions: ['css', 'scss', 'sass'],
+		},
+	],
 };
