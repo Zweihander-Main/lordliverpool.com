c9bf0d8024c47388f9b8c1367433556b7ef5cc82
Zweihänder <zweidev@zweihander.me>
2021-11-27 22:25:23 -0500

    build(package): add in gatsby-plugin-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,8 +29,9 @@ module.exports = {
 			},
 		},
 		`gatsby-plugin-remove-fingerprints`,
-		`gatsby-transformer-sharp`,
+		`gatsby-plugin-image`,
 		`gatsby-plugin-sharp`,
+		`gatsby-transformer-sharp`,
 		{
 			resolve: `gatsby-plugin-manifest`,
 			options: {
