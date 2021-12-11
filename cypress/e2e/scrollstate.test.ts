b8682e1a5c029636d07e5a900da1d43e5132bf12
Zweihänder <zweidev@zweihander.me>
2021-12-11 16:57:19 -0500

    test(scrollstate): add in back and forth tests

--- cypress/e2e/scrollstate.test.ts
+++ cypress/e2e/scrollstate.test.ts
@@ -1,70 +1,161 @@
+import _ from 'cypress/types/lodash';
+
 describe('Scroll state', () => {
 	beforeEach(() => {});
 
-	const contendersSetup = () => {
-		cy.visit('/contenders').get('main');
-		cy.findByText(/^Theresa May$/).as('theresa');
-		cy.findByTestId('caption')
-			.as('caption')
-			.should('have.text', 'Sir Robert Walpole');
-		cy.get('@theresa').click();
+	const verifyLocation = (path: string) => {
 		cy.location().should((loc) => {
-			expect(loc.href).to.include('theresa-may');
+			expect(loc.pathname).to.be.oneOf([path, `${path}/`]);
 		});
 	};
 
-	const contendersVerify = () => {
-		cy.findByTestId('caption').should('have.text', 'Theresa May');
-	};
+	context('under chronology', () => {
+		const chronologySetup = () => {
+			cy.visit('/chronology').get('main');
+			cy.findByText(/^Colleagues$/).as('colleagues');
+			cy.findByText(/^Robert Peel$/).as('peel');
+			cy.get('@colleagues').click();
+			cy.get('@peel').click();
+			verifyLocation('/chronology/robert-peel');
+		};
 
-	it('is preserved on contenders with back button', () => {
-		contendersSetup();
-		cy.go('back');
-		contendersVerify();
-	});
-
-	it('is preserved on contenders with back link', () => {
-		contendersSetup();
-		cy.findByText(/^Back to Contenders$/).as('back-button');
-		cy.get('@back-button').click();
-		contendersVerify();
-	});
-
-	const chronologySetup = () => {
-		cy.visit('/chronology').get('main');
-		cy.findByText(/^Colleagues$/).as('colleagues');
-		cy.findByText(/^Robert Peel$/).as('peel');
-		cy.get('@colleagues').click();
-		cy.get('@peel').click();
-		cy.location().should((loc) => {
-			expect(loc.href).to.include('robert-peel');
+		const chronologyVerifyState = () => {
+			verifyLocation('/chronology');
+			cy.findByText(/^Robert Peel$/)
+				.as('peel')
+				.should('be.visible');
+			cy.findByText(/^Charles Jenkinson$/).should('not.be.visible');
+		};
+		it('is preserved with user back', () => {
+			chronologySetup();
+			cy.go('back');
+			chronologyVerifyState();
 		});
-	};
 
-	const chronologyVerify = () => {
-		cy.findByText(/^Robert Peel$/)
-			.as('peel')
-			.should('be.visible');
-		cy.findByText(/^Charles Jenkinson$/).should('not.be.visible');
-	};
+		it('is preserved with back link', () => {
+			chronologySetup();
+			cy.findByText(/^Back to Chronology$/).as('back-button');
+			cy.get('@back-button').click();
+			chronologyVerifyState();
+		});
 
-	it('is preserved on chronology with back link', () => {
-		chronologySetup();
-		cy.go('back');
-		chronologyVerify();
+		it('resets context when navigating between entries', () => {
+			cy.visit('/chronology').get('main');
+			cy.findByText(/^Family$/)
+				.as('family')
+				.click();
+			cy.findByText(/^Amelia Watts Jenkinson$/)
+				.as('amelia')
+				.click();
+			verifyLocation('/chronology/amelia-watts-jenkinson');
+			cy.findByText(/^John Scott, 1st Earl of Eldon$/)
+				.as('next')
+				.click();
+			verifyLocation('/chronology/john-scott-1st-earl-of-eldon');
+			cy.findByText(/^Back to Chronology$/)
+				.as('back-button')
+				.click();
+			verifyLocation('/chronology');
+			cy.findByText(/^John Scott, 1st Earl of Eldon$/).should(
+				'be.visible'
+			);
+			cy.findByText(/^Amelia Watts Jenkinson$/).should('be.visible');
+		});
+
+		it('navigates to particular entry when no previous data held', () => {
+			cy.visit('/chronology/john-scott-1st-earl-of-eldon').get('main');
+			cy.findByText(/^Back to Chronology$/)
+				.as('back-button')
+				.click();
+			verifyLocation('/chronology');
+			cy.findByText(/^John Scott, 1st Earl of Eldon$/).should(
+				'be.visible'
+			);
+			cy.findByText(/^Amelia Watts Jenkinson$/).should('be.visible');
+		});
+
+		it('maintains state even between different types of navigation', () => {
+			cy.visit('/chronology').get('main');
+			cy.findByText(/^Family$/)
+				.as('family')
+				.click();
+			cy.findByText(/^Louisa, Countess of Liverpool$/)
+				.as('louisa')
+				.click();
+			verifyLocation('/chronology/louisa-countess-of-liverpool');
+			cy.go('back');
+			verifyLocation('/chronology');
+			cy.findByText(/^Amelia Watts Jenkinson$/)
+				.as('amelia')
+				.click();
+			verifyLocation('/chronology/amelia-watts-jenkinson');
+			cy.findByText(/^Back to Chronology$/)
+				.as('back-button')
+				.click();
+			verifyLocation('/chronology');
+			cy.findByText(/^Amelia Watts Jenkinson$/).should('be.visible');
+			cy.findByText(/^Louisa, Countess of Liverpool$/).should(
+				'not.be.visible'
+			);
+			cy.findByText(/^John Scott, 1st Earl of Eldon$/).should(
+				'be.visible'
+			);
+		});
+
+		it('maintains state on page reload', () => {
+			chronologySetup();
+			cy.go('back');
+			chronologyVerifyState();
+			cy.reload().get('main');
+			chronologyVerifyState();
+		});
 	});
 
-	it('is preserved on chronology with back link', () => {
-		chronologySetup();
-		cy.findByText(/^Back to Chronology$/).as('back-button');
-		cy.get('@back-button').click();
-		chronologyVerify();
-	});
+	context('under contenders', () => {
+		const contendersSetup = () => {
+			cy.visit('/contenders').get('main');
+			cy.findByText(/^Theresa May$/).as('theresa');
+			cy.findByTestId('caption')
+				.as('caption')
+				.should('have.text', 'Sir Robert Walpole');
+			cy.get('@theresa').click();
+			verifyLocation('/contenders/theresa-may');
+		};
 
-	//TODO: test start on post and go back
-	// TODO: go to single post, move a few, use back button
-	// TODO: louisa -> arbuthnot test
-	// TODO: click on one, then go back, then click on another, then press back button
+		const contendersVerifyCaption = () => {
+			verifyLocation('/contenders');
+			cy.findByTestId('caption').should('have.text', 'Theresa May');
+		};
+
+		it('is preserved with user back', () => {
+			contendersSetup();
+			cy.go('back');
+			contendersVerifyCaption();
+		});
+
+		it('is preserved with back link', () => {
+			contendersSetup();
+			cy.findByText(/^Back to Contenders$/).as('back-button');
+			cy.get('@back-button').click();
+			contendersVerifyCaption();
+		});
+
+		it('navigates to particular entry when no previous data held', () => {
+			cy.visit('/contenders/theresa-may').get('main');
+			cy.findByText(/^Back to Contenders$/)
+				.as('back-button')
+				.click();
+			contendersVerifyCaption();
+		});
+
+		it('maintains caption on page reload', () => {
+			contendersSetup();
+			cy.go('back');
+			contendersVerifyCaption();
+			cy.reload().get('main');
+			contendersVerifyCaption();
+		});
+	});
 });
 
 export {};
