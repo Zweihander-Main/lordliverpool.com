1377b18118844fe7b23eb2b78ccbe42b76fe56c5
Zweihänder <zweidev@zweihander.me>
2021-12-18 22:36:09 -0500

    feat(contenders): double tap for touch

--- src/components/contenders/contenderItem/index.tsx
+++ src/components/contenders/contenderItem/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'gatsby';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import * as styles from './contendersItem.module.scss';
 
 type ContendersMenuProps = {
@@ -14,10 +14,46 @@ const ContendersItem = React.forwardRef<
 	HTMLLIElement | null,
 	ContendersMenuProps
 >(({ id, isSelected, title, setSelected, slug }, ref) => {
+	const handleTouchStart = useCallback(
+		(e: React.TouchEvent<HTMLAnchorElement>, id: string) => {
+			e.preventDefault();
+			setSelected(id);
+		},
+		[setSelected]
+	);
+
+	const [preventClick, setPreventClick] = useState(false);
+
+	const handleClick: React.MouseEventHandler<HTMLAnchorElement> = useCallback(
+		(e) => {
+			if (preventClick) {
+				e.preventDefault();
+			}
+		},
+		[preventClick]
+	);
+
+	const handlePointerDown: React.PointerEventHandler<HTMLAnchorElement> =
+		useCallback(
+			(e) => {
+				if (e.nativeEvent.pointerType !== 'mouse' && !isSelected) {
+					setPreventClick(true);
+				} else {
+					setPreventClick(false);
+				}
+			},
+			[isSelected]
+		);
+
+	// TODO UI indicator for double tap
+
 	return (
 		<li className={styles.item} ref={ref}>
 			<Link
 				onMouseEnter={() => setSelected(id)}
+				onTouchStart={(e) => handleTouchStart(e, id)}
+				onPointerDown={handlePointerDown}
+				onClick={handleClick}
 				to={slug}
 				className={
 					isSelected
