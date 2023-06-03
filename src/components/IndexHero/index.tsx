b6b79787846ce1c01a48784133b828646c8dd92e
Zweihänder <zweidev@zweihander.me>
2023-06-03 16:22:34 -0400

    perf(hero): switch to static image

--- src/components/IndexHero/index.tsx
+++ src/components/IndexHero/index.tsx
@@ -1,37 +1,26 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import { GatsbyImage } from 'gatsby-plugin-image';
+import { StaticImage } from 'gatsby-plugin-image';
 import ModalButton from '../../components/RetailersButton';
 
 import * as styles from './IndexHero.module.scss';
 
 const Hero: React.FC = () => {
-	const heroImageData = useStaticQuery<Queries.HeroImageQuery>(graphql`
-		query HeroImage {
-			heroImage: file(relativePath: { eq: "liverpoolherobg.png" }) {
-				childImageSharp {
-					gatsbyImageData(layout: FULL_WIDTH, quality: 90)
-				}
-			}
-		}
-	`);
-
-	if (!heroImageData.heroImage?.childImageSharp?.gatsbyImageData) {
-		throw new Error('No file found for liverpoolherobg.png');
-	}
-
 	return (
 		<section className={styles.hero}>
-			<GatsbyImage
+			<StaticImage
 				alt="Britain's Greatest Prime Minister: Lord Liverpool"
-				image={heroImageData.heroImage.childImageSharp.gatsbyImageData}
+				src="../../images/liverpoolherobg.png"
+				layout="fullWidth"
+				quality={90}
 				className={styles.heroImage}
+				aspectRatio={1571 / 2164}
 				style={{
 					position: 'absolute',
 				}}
 				imgStyle={{
 					objectFit: 'contain',
 					objectPosition: 'center 40%',
+					aspectRatio: 'auto 1571 / 2164',
 				}}
 			/>
 			<h1 className={styles.mainTitle}>
